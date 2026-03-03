b65fe3d74a73140ec0e5cd821f385f494a890539
ssouy <ssouy@AONARI116>
2026-03-03 12:03:27 +0900

    skyboll

--- SkyBall.cpp
+++ SkyBall.cpp
@@ -14,7 +14,7 @@ using namespace DirectX;
 
 #include "SkyBall.h"
 
-#define SKYBALL_ROTATION_SPEED	(2.0f / 60.0f)  // １フレーム当たりの回転角度
+#define SKYBALL_ROTATION_SPEED	(10.0f / 60.0f)  // １フレーム当たりの回転角度
 
 static SkyBallObject		g_SkyBall;	// BALLオブジェクト
 
@@ -46,7 +46,7 @@ void SkyBall_Initialize(ID3D11Device* pDevice, ID3D11DeviceContext* pContext)
 
 	g_SkyBall.Scaling = XMFLOAT3(20.0f, 20.0f, 20.0f);
 
-	g_SkyBall.Model = ModelLoad("asset\\model\\Skyv2.fbx");
+	g_SkyBall.Model = ModelLoad("asset\\model\\Modelsky01_v6.fbx");
 
 	if (!g_SkyBall.Model)
 
